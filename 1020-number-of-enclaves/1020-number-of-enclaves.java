fa42ca9fd534e20b53ce9373b7160b0759fc379f
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2022-09-11 13:00:49 +0530

    Time: 7 ms (67.65%), Space: 54.2 MB (87.43%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -13,9 +13,7 @@ class Solution {
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    grid[i][j]=1;
-                }else if(grid[i][j]==1){
+               if(grid[i][j]==1){
                     count++;
                 }
             }
@@ -24,7 +22,7 @@ class Solution {
     }
     void DFSMarking(int[][] grid,int i,int j){
         if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!=1) return;
-        grid[i][j]=2;
+        grid[i][j]=0;
         DFSMarking(grid,i+1,j);
         DFSMarking(grid,i-1,j);
         DFSMarking(grid,i,j+1);
