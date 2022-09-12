430c37bbecfe6519ea47fdfa76fa499363a21eab
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2022-09-12 18:12:23 +0530

    Time: 230 ms (35.53%), Space: 117.3 MB (17.79%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,25 +1,20 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-       Set<String> set = new HashSet<>(wordList);
+        HashSet<String> set = new HashSet<>(wordList);
         if(!set.contains(endWord)) return 0;
-        
         Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
-        
-        int changes = 1;
-        
+        int level=1;
         while(!queue.isEmpty()){
             int size = queue.size();
-            for(int i = 0; i < size; i++){
-                String word = queue.poll();
-                if(word.equals(endWord)) return changes;
-                
-                for(int j = 0; j < word.length(); j++){
-                    for(int k = 'a'; k <= 'z'; k++){
-                        char arr[] = word.toCharArray();
-                        arr[j] = (char) k;
-                        
-                        String str = new String(arr);
+            while(size-->0){
+                String curr = queue.poll();
+                if(curr.equals(endWord)) return level;
+                for(int i=0;i<curr.length();i++){
+                    for(char c='a';c<='z';c++){
+                        char temp[] = curr.toCharArray();
+                        temp[i]=(char)c;
+                        String str = new String(temp);
                         if(set.contains(str)){
                             queue.add(str);
                             set.remove(str);
@@ -27,8 +22,8 @@ class Solution {
                     }
                 }
             }
-            ++changes;
+            level++;
         }
-        return 0; 
+        return 0;
     }
 }
