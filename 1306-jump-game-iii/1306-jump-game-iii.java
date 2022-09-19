9358eb86eb64ae678716f2d754c3632b6af864c0
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2022-09-19 14:40:51 +0530

    Time: 4 ms (85.79%), Space: 61.5 MB (50.73%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+          
+        return 0 <= start && start < arr.length && arr[start] >= 0 && ((arr[start] = -arr[start]) == 0 || canReach(arr,start-arr[start]) || canReach(arr,start+arr[start]));
+    }
+}
