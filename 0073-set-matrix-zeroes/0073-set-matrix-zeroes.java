6dc070ee54b9beb11ca94bb477d465dcbf25a62e
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-01-14 16:50:14 +0530

    Time: 1 ms (89.84%), Space: 43.7 MB (86.78%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        // Approach:
+        // 1. Naive
+        //     tc: O((N*M)*(N+M))+O(N*M)
+        // 2. Better
+        //     tc: O(N*M)+O(N*M)
+        //     sc: O(N+M)
+        // 3. Optimized
+        //     tc: O(N*M)+O(N*M)
+        
+        int col0=1;int n = matrix.length,m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0) col0=0;
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+                
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(col0==0) matrix[i][0]=0;
+        }   
+    }
+}
