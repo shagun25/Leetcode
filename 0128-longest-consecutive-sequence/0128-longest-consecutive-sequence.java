d4f8218378a03e406b7c7b37025cbc91096d1127
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-01-14 16:30:35 +0530

    Time: 365 ms (42.55%), Space: 59.1 MB (77.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,5 +1,6 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // Approach: We will first push all are elements in the HashSet. Then we will run a for loop and check for any number(x) if it is the starting number of the consecutive sequence by checking if the HashSet contains (x-1) or not. If ‘x’ is the starting number of the consecutive sequence we will keep searching for the numbers y = x+1, x+2, x+3, ….. And stop at the first ‘y’ which is not present in the HashSet. Using this we can calculate the length of the longest consecutive subsequence. 
         int n = nums.length;
         if(n==0) return 0;
         if(n==1) return 1;
