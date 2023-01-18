2fcec86b497b4273a7c6735b7adcf66daa3115b9
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-01-18 21:42:46 +0530

    Time: 6 ms (54.32%), Space: 47.3 MB (84.47%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,22 +1,4 @@
 class Solution {
-    int maxSubarraySumKadane(int[] nums){
-         int N = nums.length;
-        int sum=0,max=Integer.MIN_VALUE;
-         for(int i=0;i<N;i++){
-             sum=Math.max(sum+nums[i],nums[i]);
-             max=Math.max(sum,max);
-         }
-        return max;
-    }
-     int minSubarraySumKadane(int[] nums){
-         int N = nums.length;
-        int sum=0,min=Integer.MAX_VALUE;
-         for(int i=0;i<N;i++){
-             sum=Math.min(sum+nums[i],nums[i]);
-             min=Math.min(sum,min);
-         }
-        return min;
-    }
     public int maxSubarraySumCircular(int[] nums) {
         // Case 1: All are negative 
         //     ans will be one max of al negative -> return normal maxKadane
@@ -24,10 +6,16 @@ class Solution {
         //     case 2.1: max Subbarray can be one side -> return normal maxKadane
         //     case 2.2: max Subbarray both the edge sides -> return (totalSum-minKadane)
         int totalSum=0;
-        for(int x:nums) totalSum+=x;
-        int minKadane = minSubarraySumKadane(nums);
-        int maxKadane = maxSubarraySumKadane(nums);
-        if(totalSum==minKadane) return maxKadane;
-        return Math.max(totalSum-minKadane,maxKadane);
+        int N = nums.length;
+        int sumMin=0,max=Integer.MIN_VALUE;
+        int sumMax=0,min=Integer.MAX_VALUE;
+        for(int i=0;i<N;i++){
+             sumMin=Math.max(sumMin+nums[i],nums[i]);
+             max=Math.max(sumMin,max);
+             sumMax=Math.min(sumMax+nums[i],nums[i]);
+             min=Math.min(sumMax,min);
+             totalSum+=nums[i];
+        }
+        return (totalSum==min)?max:Math.max(totalSum-min,max);
     }
 }
