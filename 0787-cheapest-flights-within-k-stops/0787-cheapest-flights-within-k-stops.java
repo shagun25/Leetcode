5fac1e3711f21f52977ca0ab4cb309aa2640e452
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-01-26 15:59:41 +0530

    Time: 7 ms (58.06%), Space: 43.2 MB (77.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,21 +1,31 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // BellmanFord algo:
-        // src to all the other vertcies shortest path calc.
-        //     twist here: normally outer loop run for v-1 times but here k times beacuse we want shorttest path after certain number of edge gap to the required dst
-        int prev[] = new int[n];
-        Arrays.fill(prev,Integer.MAX_VALUE);
-        prev[src]=0;
-        for(int i=0;i<=k;i++){
-            int curr[] = Arrays.copyOf(prev,n);
-            for(int f[]:flights){
-                int u = f[0], v = f[1], wt = f[2];
-                if(prev[u]!=Integer.MAX_VALUE){
-                    if(curr[v]>prev[u]+wt) curr[v]=prev[u]+wt;
+        Map<Integer,List<int[]>> map = new HashMap<>();
+        for(int[] f: flights){
+            map.computeIfAbsent(f[0],value->new ArrayList<>()).add(new int[]{f[1],f[2]});
+        }
+        int dist[] = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{src,0});
+        int stops=0;
+        while(stops<=k && !q.isEmpty()){
+            int sz = q.size();
+            while(sz-->0){
+                int temp[] = q.poll();
+                int node = temp[0],distance=temp[1];
+                if(map.containsKey(node)){
+                    for(int[] e:map.get(node)){
+                        int v=e[0], wt=e[1];
+                        if(wt+distance<dist[v]){
+                            dist[v]=wt+distance;
+                            q.add(new int[]{v,dist[v]});
+                        }
+                    }
                 }
             }
-            prev = curr;
+            stops++;
         }
-        return (prev[dst]==Integer.MAX_VALUE)?-1:prev[dst];
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
     }
 }
