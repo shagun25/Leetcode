ca18bbd5b1c825da06ba50343cb9806508caadb1
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-01-27 12:05:27 +0530

    Time: 65 ms (73.27%), Space: 85.9 MB (15.27%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,42 +1,36 @@
 class Solution {
-    private static Node root;
+     Node root;
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        if (words == null || words.length == 0)
-            return new ArrayList<>();
+        List<String> res= new LinkedList<>();
+        if(words==null || words.length==0) return res;
         root = new Node();
         buildTrie(words);
-        List<String> res = new LinkedList<>();
         for(String word : words){
             if(isConcatenated(word,0,0)) res.add(word);
         }
         return res;
     }
-    boolean isConcatenated(String word,int index,int count){
+    boolean isConcatenated(String word, int index, int count){
         if(index==word.length()) return count>=2;
         Node ptr = root;
-        for(int i=index;i<word.length();i++){
-            if(ptr.children[word.charAt(i) - 'a']==null) return false;
-            ptr = ptr.children[word.charAt(i) - 'a'];
-            
-            if (ptr.isEnd)
-                if (isConcatenated(word, i + 1, count + 1))
-                    return true;
+        for(int i = index;i<word.length();i++){
+            if(ptr.children[word.charAt(i)-'a']==null) return false;
+            ptr = ptr.children[word.charAt(i)-'a'];
+            if(ptr.isEnd){
+                if(isConcatenated(word,i+1,count+1)) return true;
+            }
         }
         return false;
     }
-    
     void buildTrie(String[] words){
         Node ptr;
         for(String word : words){
             ptr = root;
             for(char ch : word.toCharArray()){
-                int index = ch-'a';
-                if(ptr.children[index]==null){
-                    ptr.children[index] = new Node();
-                }
-                ptr = ptr.children[index];
+                if(ptr.children[ch-'a']==null) ptr.children[ch-'a'] = new Node();
+                ptr=ptr.children[ch-'a'];
             }
-            ptr.isEnd = true;
+            ptr.isEnd=true;
         }
     }
     class Node{
