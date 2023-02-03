b5b00b4939a0c7b6f5c74e33e612509a88645a1d
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-02-03 15:38:42 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (80.48%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,25 +1,23 @@
 class Solution {
-    public boolean isAlienSorted(String[] words, String order) {
-        int[] orderMap = new int[26];
-        for (int i = 0; i < order.length(); i++){
-            orderMap[order.charAt(i) - 'a'] = i;
+    public boolean isAlienSorted(String[] words, String orders) {
+        int order[] = new int[26];
+        for(int i=0;i<orders.length();i++){
+            order[orders.charAt(i)-'a']=i;
         }
-
-        for (int i = 0; i < words.length - 1; i++) {
-
-            for (int j = 0; j < words[i].length(); j++) {
-                if (j >= words[i + 1].length()) return false;
-
+        
+        for(int i=0;i<words.length-1;i++){
+            for(int j=0;j<words[i].length();j++){
+                if(j>=words[i+1].length()) return false;
                 if (words[i].charAt(j) != words[i + 1].charAt(j)) {
-int currentWordChar = words[i].charAt(j) - 'a';
-int nextWordChar = words[i + 1].charAt(j) - 'a';
-                    if (orderMap[currentWordChar] > orderMap[nextWordChar]) return false;
-                   
+                    int currChar = words[i].charAt(j)-'a';
+                    int nextChar = words[i+1].charAt(j)-'a';
+                    if(order[nextChar]<order[currChar]) return false;
                     else break;
                 }
             }
         }
-
+        
         return true;
     }
+ 
 }
