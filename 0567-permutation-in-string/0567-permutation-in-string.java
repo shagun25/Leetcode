8d1008189edb8e744a4df33e5ff47f51f5c7f056
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-02-04 15:03:02 +0530

    Time: 961 ms (13.54%), Space: 42.3 MB (72.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,16 @@
 public class Solution {
-//      a b
-//      1 1
-         
-//      e i d b a o 
-//      1 1 1 1 1 3 -> false
-     
-//      i d b a o
-//      1 1 1 1 2 -> false
-    
-//      d b a o
-//      1 1 1 1 -> false
-         
-//      b a o
-//      1 1 1 -> true
-         
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1map = new int[26];
-        for (int i = 0; i < s1.length(); i++)
-            s1map[s1.charAt(i) - 'a']++;
+        s1 = sort(s1);
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            int[] s2map = new int[26];
-            for (int j = 0; j < s1.length(); j++) {
-                s2map[s2.charAt(i + j) - 'a']++;
-            }
-            if (matches(s1map, s2map))
+            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
                 return true;
         }
         return false;
     }
     
-    public boolean matches(int[] s1map, int[] s2map) {
-        for (int i = 0; i < 26; i++) {
-            if (s1map[i] != s2map[i])
-                return false;
-        }
-        return true;
+    public String sort(String s) {
+        char[] t = s.toCharArray();
+        Arrays.sort(t);
+        return new String(t);
     }
 }
