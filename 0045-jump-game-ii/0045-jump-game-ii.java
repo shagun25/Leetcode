95950c760bdf4f7804a6ec4e25e329ea30048be4
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-02-09 00:15:09 +0530

    Time: 1 ms (99.45%), Space: 42.9 MB (48.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,26 +1,21 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums == null || nums.length <2) return 0;
+        // The starting range of the first jump is [0, 0]
+        int answer = 0, n = nums.length;
+        int curEnd = 0, curFar = 0;
         
-        int[] memData = new int[nums.length];
-        memData[nums.length-1] = 0;
-        
-        for(int i=nums.length-1; i>=0; i--){ 
-            if (i == nums.length -1)
-                continue;
-            if(nums[i] == 0){
-                memData[i] = Integer.MAX_VALUE;
-                continue;
+        for (int i = 0; i < n - 1; ++i) {
+            // Update the farthest reachable index of this jump.
+            curFar = Math.max(curFar, i + nums[i]);
+
+            // If we finish the starting range of this jump,
+            // Move on to the starting range of the next jump.
+            if (i == curEnd) {
+                answer++;
+                curEnd = curFar;
             }
-            
-            int minSteps =  Integer.MAX_VALUE;
-            int maxIndex = Math.min(nums.length-1, i+nums[i]);
-            for(int j=i+1; j <= maxIndex; j++){
-                minSteps = Math.min(minSteps, memData[j]);
-            }
-            memData[i] = (minSteps==Integer.MAX_VALUE)? Integer.MAX_VALUE: minSteps+1;
         }
         
-        return memData[0];
+        return answer;
     }
 }
