5179031129c62c322babc895ed1d1339cf21c880
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-02-13 20:58:44 +0530

    Time: 7 ms (50.57%), Space: 42.3 MB (59.20%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,21 +1,21 @@
 class Solution {
-    public String removeOuterParentheses(String str) {
-        StringBuilder s = new StringBuilder();
-        int opened=0;
-        for(char ch : str.toCharArray()){
-            if(ch=='(' && opened++>0) s.append(ch);
-            if(ch==')' && opened-->1) s.append(ch);
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                if (st.size() >= 1) {
+                    sb.append(s.charAt(i));
+                }
+                st.push(s.charAt(i));
+            } else {
+                if (st.size() > 1) {
+                    sb.append(s.charAt(i));
+                }
+                st.pop();
+            }
         }
-        return s.toString();
+        return sb.toString();
     }
-//     Primitive string will have equal number of opened and closed parenthesis.
-
-// Explanation:
-// opened count the number of opened parenthesis.
-// Add every char to the result,
-// unless the first left parenthesis,
-// and the last right parenthesis.
-
-// Time Complexity:
-// O(N) Time, O(N) space
 }
