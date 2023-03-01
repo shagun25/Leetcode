35cc8085fcb41d359aaaeed7b130cb4a4529dcc1
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-01 22:21:51 +0530

    Time: 1 ms (100.00%), Space: 41.9 MB (34.23%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int res=0;
+        for(int i=0;i<columnTitle.length();i++){
+            char ch = columnTitle.charAt(i);
+            res=res*26+(ch-'A'+1);
+        }
+        
+        return res;
+    }
+}
