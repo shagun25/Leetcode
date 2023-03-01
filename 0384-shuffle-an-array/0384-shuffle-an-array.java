8d07b7d09713f1bc8a0e0017532767d52bf61362
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-01 21:56:31 +0530

    Time: 55 ms (34.28%), Space: 48 MB (83.76%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.java
+++ 0384-shuffle-an-array/0384-shuffle-an-array.java
@@ -1,40 +1,33 @@
-class Solution {
-    //Fisher-Yates Algorithm 
-    private int[] array;
-    private int[] original;
-    
-    Random rand = new Random();
-    
-    private int randRange(int min, int max) {
-        return rand.nextInt(max - min) + min;
-    }
-    private void swapAt(int i,int j){
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+import java.util.Random;
+
+public class Solution {
+    private int[] nums;
+    private Random random;
+
     public Solution(int[] nums) {
-        array = nums;
-        original = nums.clone();
+        this.nums = nums;
+        random = new Random();
     }
     
+    /** Resets the array to its original configuration and return it. */
     public int[] reset() {
-        array = original;
-        original = original.clone();
-        return original;
+        return nums;
     }
     
+    /** Returns a random shuffling of the array. */
     public int[] shuffle() {
-        for(int i=0;i<array.length;i++){
-            swapAt(i,randRange(i,array.length));
+        if(nums == null) return null;
+        int[] a = nums.clone();
+        for(int j = 1; j < a.length; j++) {
+            int i = random.nextInt(j + 1);
+            swap(a, i, j);
         }
-        return array;
+        return a;
+    }
+    
+    private void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int[] param_1 = obj.reset();
- * int[] param_2 = obj.shuffle();
- */
