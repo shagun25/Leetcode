f3cab810eecd67e55fbec1108287672c4fa180d8
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-06 22:27:01 +0530

    Time: 2 ms (75.17%), Space: 43.2 MB (24.10%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReachable=0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(maxReachable<i) return false;
+            maxReachable=Math.max(maxReachable,i+nums[i]);
+        }
+        return true;
+    }
+}
