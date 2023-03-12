f6fbaef54f7787206ca11fc4369c3fb9900db138
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-12 13:01:03 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (13.92%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -39,32 +39,70 @@ class Solution {
     // }
     
     //Method3 Compare K elements by Priority Queue
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         int n = lists.length;
+//         if (lists == null || n == 0) return null;
+        
+//         // Ensure our pq takes in ListNodes and compares their values.
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((ListNode a, ListNode b) -> a.val - b.val);
+        
+//         // Add the head of all non-null lists to the pq to begin.
+//         for (ListNode list: lists)
+//             if (list != null) pq.add(list);
+        
+//         // Dummy node pointer to help add to our output list.
+//         ListNode ptr = new ListNode(-1);
+//         ListNode head = ptr;
+        
+//         // 1) Get the smallest node from the PriorityQueue.
+//         // 2) Add it to our output list.
+//         // 3) Add the next node of the current list to the pq if it's not null
+//         // 4) Repeat
+//         while (!pq.isEmpty()) {
+//             ListNode curr = pq.poll();
+//             ptr.next = new ListNode(curr.val);
+//             ptr = ptr.next;
+//             if (curr.next != null) pq.add(curr.next);
+//         }
+        
+//         return head.next;    
+//     }
+    
+    // Method 5: Merge K lists by Divide & Conquer (Most Optimal)
     public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        if (lists == null || n == 0) return null;
-        
-        // Ensure our pq takes in ListNodes and compares their values.
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((ListNode a, ListNode b) -> a.val - b.val);
-        
-        // Add the head of all non-null lists to the pq to begin.
-        for (ListNode list: lists)
-            if (list != null) pq.add(list);
-        
-        // Dummy node pointer to help add to our output list.
-        ListNode ptr = new ListNode(-1);
-        ListNode head = ptr;
-        
-        // 1) Get the smallest node from the PriorityQueue.
-        // 2) Add it to our output list.
-        // 3) Add the next node of the current list to the pq if it's not null
-        // 4) Repeat
-        while (!pq.isEmpty()) {
-            ListNode curr = pq.poll();
-            ptr.next = new ListNode(curr.val);
-            ptr = ptr.next;
-            if (curr.next != null) pq.add(curr.next);
+        if(lists.length == 0) return null;
+        if(lists.length == 1) return lists[0];
+        return helper(lists, 0, lists.length-1);
+    }
+
+    public ListNode helper(ListNode[] lists, int left, int right){
+        if(left == right) return lists[left];
+
+        int mid = left + (right-left)/2;
+        ListNode first = helper(lists, left, mid);
+        ListNode second = helper(lists, mid+1, right);
+        return merge(first, second);
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2){
+        ListNode temp = new ListNode(0);
+        ListNode p = temp;
+        while(list1!=null && list2!=null){
+            if(list1.val < list2.val){
+                p.next = list1;
+                list1 = list1.next;
+            }else{
+                p.next = list2;
+                list2 = list2.next;
+            }
+            p = p.next;
         }
-        
-        return head.next;    
+        if(list1!=null){
+            p.next = list1;
+        }
+        if(list2!=null){
+            p.next = list2;
+        }
+        return temp.next;
     }
 }
