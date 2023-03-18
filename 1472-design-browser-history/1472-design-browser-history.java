24404db0248c4307d8579665c6d6cac69102559d
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-18 09:46:08 +0530

    Time: 57 ms (67.07%), Space: 50.5 MB (53.18%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,45 +1,38 @@
 class BrowserHistory {
-    Stack<String> history,future;
-    String current;
+    ArrayList<String> visitedURl;
+    int currUrl,lastURL;
     public BrowserHistory(String homepage) {
-        history= new Stack<>();
-        future = new Stack<>();
-        current = homepage;
+        visitedURl = new  ArrayList<String>(Arrays.asList(homepage));
+        currUrl=0;
+        lastURL=0;
     }
     
     public void visit(String url) {
-        history.push(current);
-        current=url;
-        future = new Stack<>();
+        currUrl+=1;
+        if(visitedURl.size()>currUrl){
+            visitedURl.set(currUrl,url);
+        }else{
+            visitedURl.add(url);
+        }
+        lastURL = currUrl;
     }
     
     public String back(int steps) {
-        while(steps>0 && !history.isEmpty()){
-            future.push(current);
-            current = history.pop();
-            steps--;
-        }
-        return current;
+        currUrl = Math.max(0,currUrl-steps);
+        return visitedURl.get(currUrl);
     }
     
     public String forward(int steps) {
-        while(steps>0 && !future.isEmpty()){
-            history.push(current);
-            current = future.pop();
-            steps--;
-        }
-        return current;
+        currUrl = Math.min(lastURL,currUrl+steps);
+        return visitedURl.get(currUrl);
     }
 }
-// Let's assume here, n visit calls are made, m is the maximum number of steps to go forward or back, and l is the maximum length of the URL string.
-// Time complexity: 
-// visit(url) 
-//     in the worst case each call to the visit(url) method will take O(1) time.
-// back(steps) and forward(steps)
-//     in the worst case, each call to these methods will take O(min(m,n)) time.
-
-// Space complexity: n the worse case, we use O(l⋅n) space.
-
+// Let's assume here, n visit calls are made, m is the maximum number of steps to go forward or back, and l is the maximum length of a URL string.
+// Time complexity:
+// visit(url) ->   insert and update O(1)
+// back(steps) and forward(steps) -> O(1) time.
+// Space complexity:  O(l⋅n) space.
+     
 /**
  * Your BrowserHistory object will be instantiated and called as such:
  * BrowserHistory obj = new BrowserHistory(homepage);
