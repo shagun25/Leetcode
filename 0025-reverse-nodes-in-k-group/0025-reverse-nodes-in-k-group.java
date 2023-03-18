34ca3aae7f793a38d2566249299f51dbb3d9ffb1
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-18 11:26:46 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (27.76%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+// This problem can be split into several steps:
+
+// Since we need to reverse the linked-list every k nodes, we need to check whether the number of list nodes are enough to reverse. Otherwise, there is no need to reverse.
+
+// If we need to reverse the k nodes, how to do that? O(k.n)
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pointer = dummy;
+        while (pointer != null) {
+            ListNode node = pointer;
+            // first check whether there are k nodes to reverse
+            for (int i = 0; i < k && node != null; i++) {
+                node = node.next;
+            }
+            if (node == null) break;
+            
+            // now we know that we have k nodes, we will start from the first node
+            ListNode prev = null, curr = pointer.next, next = null;
+            for (int i = 0; i < k; i++) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            ListNode tail = pointer.next;
+            tail.next = curr;
+            pointer.next = prev;
+            pointer = tail;
+        }
+        return dummy.next;
+    }
+}
