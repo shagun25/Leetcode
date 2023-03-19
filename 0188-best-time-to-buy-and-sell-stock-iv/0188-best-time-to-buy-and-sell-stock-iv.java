bd170ae3a72228840764c3071227b85031b3c97a
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-19 18:41:59 +0530

    Time: 107 ms (11.23%), Space: 42.2 MB (56.75%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,10 +1,12 @@
 class Solution {
+    
     public int maxProfit(int k, int[] prices) {
         //Dp
         int n = prices.length;
         if(n<2) return 0;//only one stock, buy sell on same day=0.
-        int dp[][] = new int[k+1][n];
+        int dp[][] = new int[k+1][n]; // space: O(k*n)
         
+        //TC: O(k*n*n)
         for(int i=1;i<=k;i++){
             for(int j=1;j<n;j++){
                 dp[i][j] = Math.max(dp[i][j-1],helper(dp,prices,i,j));
@@ -12,10 +14,11 @@ class Solution {
         }
         return dp[k][n-1];
     }
-    int helper(int dp[][], int[] prices, int k, int day){
+    int helper(int dp[][], int[] prices, int k, int day_x){
         int max=0;
-        for(int x=0;x<day;x++){
-            max=Math.max(max,dp[k-1][x]+prices[day]-prices[x]);
+        for(int x=0;x<day_x;x++){
+            //sell on day_x, buy on day x, and add profit from i-1 transactions i.e dp[k-1][x]
+            max=Math.max(max,dp[k-1][x]+prices[day_x]-prices[x]);
         }
         return max;
     }
