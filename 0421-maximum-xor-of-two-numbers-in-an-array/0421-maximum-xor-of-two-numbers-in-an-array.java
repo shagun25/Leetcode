44fe4bcafebc64f5d05552ca9d5d9af4a909d802
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-26 22:25:11 +0530

    Time: 906 ms (33.98%), Space: 75.4 MB (96.66%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,59 +1,21 @@
-class TrieNode{
-    TrieNode links[];
-    TrieNode(){
-        links = new TrieNode[2];
-    }
-    public boolean containsKey(int indx){
-        return links[indx]!=null;
-    }
-    public TrieNode get(int indx){
-        return links[indx];
-    }
-    public void put(int indx, TrieNode node){
-        links[indx] = node;
-    }
-}
-class Trie {
-    TrieNode root;
-    public Trie() {
-        root = new TrieNode();
-    }
-    
-    public void insert(int num) {
-        TrieNode node = root;
-        for(int i=31;i>=0;i--){
-            int bit = (num>>i)&1;
-            if(!node.containsKey(bit)){
-                node.put(bit, new TrieNode());
-            }
-            node = node.get(bit);
-        }
-    }
-    public int getMax(int num){
-        TrieNode node = root;
-        int maxNum=0;
-        for(int i=31;i>=0;i--){
-            int bit = (num>>i)&1;
-            if(node.containsKey(1-bit)){
-                maxNum = maxNum | (1<<i);
-                node = node.get(1-bit);
-            }
-            else{
-               node = node.get(bit); 
-            }
-        }
-        return maxNum;
-    }
-}
 class Solution {
-    Trie trie = new Trie();
     public int findMaximumXOR(int[] nums) {
-        for(int num : nums){
-            trie.insert(num);
-        }
-        int max = -1;
-        for(int num : nums){
-            max=Math.max(max,trie.getMax(num));
+        // a^b=T
+        // a^T=b , b^T=a
+        int max = 0, mask = 0;
+        for(int i = 31; i >= 0; i--){
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for(int num : nums){
+                set.add(num & mask);
+            }
+            int tmp = max | (1 << i);
+            for(int prefix : set){
+                if(set.contains(tmp ^ prefix)) {
+                    max = tmp;
+                    break;
+                }
+            }
         }
         return max;
     }
