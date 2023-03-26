5c71268abe8a46d5bd5a7926f307b8394386efca
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-27 00:31:29 +0530

    Time: 9 ms (98.55%), Space: 59 MB (54.01%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,13 +1,75 @@
-class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        StringBuilder sb = new StringBuilder();
-        String s[] = sentence.split(" ");
-        for(String s1: s){
-            for(String s2: dictionary){
-                if(s1.startsWith(s2)) s1=s2;
-            }
-            sb.append(s1).append(" ");
-        }
-        return sb.toString().trim();
+class TrieNode{
+    TrieNode links[];
+    boolean isEnd;
+    TrieNode(){
+        links = new TrieNode[26];
+        isEnd = false;
     }
+    public boolean containsKey(char ch){
+        return links[ch-'a']!=null;
+    }
+    public TrieNode get(char ch){
+        return links[ch-'a'];
+    }
+    public void put(char ch, TrieNode node){
+        links[ch-'a'] = node;
+    }
+    public void setEnd(){
+        isEnd=true;
+    }
+    public boolean isEnd(){
+        return isEnd;
+    }
+}
+
+class Solution {
+    TrieNode root;
+    public void insert(String word) {
+        TrieNode node = root;
+        for(char ch : word.toCharArray()){
+            if(!node.containsKey(ch)){
+                node.put(ch, new TrieNode());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+    public String search(String word){
+         TrieNode node = root;
+        for(int i = 0; i < word.length(); ++i){
+            char ch = word.charAt(i);
+            
+            if(node.isEnd() == true){
+                return word.substring(0, i);
+            }
+            
+            if(node.containsKey(ch) == false){
+                return word;
+            }
+            
+            node = node.get(ch);
+        }
+        
+        return word;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        root = new TrieNode();
+        for(int i = 0; i < dictionary.size(); ++i){
+            String word = dictionary.get(i);
+            insert(word);
+        }
+        
+        String[] words = sentence.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < words.length; ++i){
+            String word = words[i];
+            String prefix = search(word);
+            res.append(prefix);
+            res.append(" ");
+        }
+        
+        return res.toString().substring(0, res.length() - 1);
+    }
+ 
 }
