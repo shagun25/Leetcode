efdb66628144e5418cf460093bfdd8eb003d7760
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-03-27 23:45:09 +0530

    Time: 178 ms (79.63%), Space: 40.9 MB (35.12%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,71 +1,26 @@
-class TrieNode{
-    TrieNode links[];
-    String wrd;
-    TrieNode(){
-        links = new TrieNode[127];
-        wrd=null;
-    }
-    boolean containsKey(char ch){
-        return links[ch]!=null;
-    }
-    void put(char ch,TrieNode node){
-        links[ch]=node;
-    }
-    TrieNode get(char ch){
-        return links[ch];
-    }
-    void setWrd(String word){
-        wrd=word;
-    }
-    String getWrd(){
-        return wrd;
-    }
-}
-class Trie{
-    TrieNode root;
-    Trie(){
-        root = new TrieNode();
-    }
-    void insert(String word){
-        TrieNode node = root;
-        for(char ch : word.toCharArray()){
-            if(!node.containsKey(ch)){
-                node.put(ch,new TrieNode());
-            }
-            node = node.get(ch);
-        }
-        node.setWrd(word);
-    }
-}
 class Solution {
     boolean flag=false;
-   void DFS(char[][] board, TrieNode node, int i,int j){
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]=='#' || !node.containsKey(board[i][j])) return;
+   void DFS(char[][] board, int i,int j,int ind,String word){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]=='#' || word.charAt(ind)!=board[i][j]) return;
         char c = board[i][j];
-      // System.out.print(c+" ");
-        node = node.get(c);
-        if(node.getWrd()!=null){
-            node.setWrd(null);
+        if(ind==word.length()-1){
             flag=true;
             return;
         }
        
         board[i][j]='#';
-        DFS(board,node,i+1,j);
-        DFS(board,node,i-1,j);
-        DFS(board,node,i,j+1);
-        DFS(board,node,i,j-1);
+        ind++;
+        DFS(board,i+1,j,ind,word);
+        DFS(board,i-1,j,ind,word);
+        DFS(board,i,j+1,ind,word);
+        DFS(board,i,j-1,ind,word);
         board[i][j]=c;
     }
     public boolean exist(char[][] board, String word) {
-        Trie trie = new Trie();
-        trie.insert(word);
-        
-        TrieNode node = trie.root;
         
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                DFS(board,node,i,j);
+                DFS(board,i,j,0,word);
             }
         }
         return flag;
