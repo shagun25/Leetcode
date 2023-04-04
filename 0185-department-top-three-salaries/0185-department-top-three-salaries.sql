ae9090d97a78cb87da0fbd0a48e40ac4ddb086fc
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-04-04 13:48:35 +0530

    Time: 1930 ms (36.12%), Space: 0B (100.00%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+select 
+    d.name as 'Department',
+    e.name as 'Employee',
+    e.salary as 'Salary'
+FROM 
+    Employee e JOIN Department d 
+ON (e.departmentId = d.id )
+where e.Salary IN(
+    select * FROM (  
+        SELECT
+            DISTINCT Salary
+        FROM
+            Employee
+        where departmentId = d.id 
+        ORDER BY Salary DESC LIMIT 3
+    ) as T
+);
+
