7719d290aca4f9540381f20c27e059ad80eb069b
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-04-04 12:40:48 +0530

    Time: 815 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,4 +2,8 @@
 
 # select MAX(salary) as  SecondHighestSalary from Employee where salary not in (select MAX(salary) from Employee);
 
-SELECT IFNULL((select DISTINCT salary from Employee ORDER BY salary DESC LIMIT 1,1),null) as SecondHighestSalary;
+# SELECT IFNULL((select DISTINCT salary from Employee ORDER BY salary DESC LIMIT 1,1),null) as SecondHighestSalary;
+
+select IFNULL((select Distinct e1.salary from Employee e1 where 1 = (
+    select COUNT(Distinct e2.salary) from Employee e2 where e2.salary>e1.salary
+)),null) as SecondHighestSalary;
