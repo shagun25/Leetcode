5d75fd5eed04c8ff1570a6fdc592d072bb5c1ac2
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-04-06 15:21:11 +0530

    Time: 909 ms (47.77%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+
+# 1. Group the table with the product_id field and find the first changed date over 2019-08-16 by using MIN aggregation function on HAVING clause.
+
+# 2. Set the price table as 10.
 SELECT
   product_id,
   10 AS price
@@ -7,7 +11,11 @@ FROM
  group by product_id
  HAVING
   MIN(change_date) > '2019-08-16'
+  
+  
 UNION ALL
+
+
 SELECT
   product_id,
   new_price AS price
@@ -21,4 +29,10 @@ WHERE
 from Products 
 where change_date<='2019-08-16' 
 group by product_id)
+
+#3. Group the table with the product_id again, and find the product_id field and the last changed date until 2019-08-16.
+#4. Find the last changed new_price field with the last changed date.
+
+# 5. Union the two tables by using UNION ALL.
+
 
