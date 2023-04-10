314589aa0b827fdaec6b8c253d294b05a27ed16c
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-04-10 23:11:18 +0530

    Time: 13 ms (33.78%), Space: 41.6 MB (51.85%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,27 @@
+class Foo {
+    private volatile int count;
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        count++;
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while(count<1);
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        count++;
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while(count<2);
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        count++;
+    }
+}
