5887a43d881e8285355df0e22824adafdf0e8770
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-04-27 09:07:23 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (31.56%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,10 +1,5 @@
 class Solution {
     public int bulbSwitch(int n) {
-        int i=1,count=0;
-        while(i*i<=n){
-            count++;
-            i++;
-        }
-        return count;
+        return (int) Math.sqrt(n);
     }
 }
