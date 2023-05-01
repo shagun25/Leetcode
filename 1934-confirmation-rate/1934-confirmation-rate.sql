4d3e888da2128d04ffc1b04ad4e85bef38df7d4b
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-05-01 16:44:18 +0530

    Time: 1560 ms (17.32%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT s.user_id , ROUND(AVG(CASE WHEN action='confirmed' THEN 1.00 ELSE 0.00 END),2) confirmation_rate from Signups s LEFT JOIN Confirmations c ON s.user_id=c.user_id GROUP BY s.user_id;
