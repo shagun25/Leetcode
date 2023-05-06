e1c3483da79bf9e295a4796b92ac3b8d893da3f8
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-05-06 12:38:41 +0530

    Time: 34 ms (37.65%), Space: 51.4 MB (35.76%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -27,3 +27,8 @@ class Solution {
         return answer;
     }
 }
+// The window calculates the number of subsequences that must start with nums[left].
+// [l = 0, r = 7] will contain all of the combinations [l = 1, r = 7], BUT appended with nums[0] in front.
+// Therefore all the elements in [l = 0, r = 7] is unique from [l = 1, r = 7].
+// remember that we must include the minimum (or at least 1) element in any subsequence in order to calc min+max???
+// it means that we calculate subsequences with fixed [3] and variable [5,6]
