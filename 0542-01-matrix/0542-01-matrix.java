548b255e00dcf7c30d739e48d69c183afebd8c68
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2023-08-17 11:34:47 +0530

    Time: 14 ms (74.76%), Space: 45.2 MB (94.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,30 +1,28 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length, m = mat[0].length;
-        int dp[][] = new int[n][m];
-        for(int d[] : dp) Arrays.fill(d,Integer.MAX_VALUE-10000);
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    dp[i][j]=0;
-                }
-                else{
-                    if(i>0) dp[i][j]=Math.min(dp[i][j],dp[i-1][j]+1);
-                    if(j>0) dp[i][j]=Math.min(dp[i][j],dp[i][j-1]+1);
+                   q.add(new int[]{i,j});
+                    vis[i][j]=true;
                 }
             }
         }
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(mat[i][j]==0){
-                    dp[i][j]=0;
-                }
-                else{
-                    if(i<n-1) dp[i][j]=Math.min(dp[i][j],dp[i+1][j]+1);
-                    if(j<m-1) dp[i][j]=Math.min(dp[i][j],dp[i][j+1]+1);
-                }
+        int dir[][] = new int[][]{{1,0},{0,-1},{-1,0},{0,1}};
+        while(!q.isEmpty()){
+            int curr[] = q.poll();
+            for(int d[] : dir){
+                int x = curr[0] + d[0];
+                int y = curr[1] + d[1];
+                if(x<0 || x>=n || y<0 || y>=m || vis[x][y]) continue;
+                mat[x][y] = 1+ mat[curr[0]][curr[1]];
+                q.add(new int[]{x,y});
+                vis[x][y]=true;
             }
         }
-        return dp;
+        return mat;
     }
 }
