456d3482019b1f8cb53ec5ec26017137e5570d33
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-06-24 02:50:49 +0530

    Time: 29 ms (77.87%), Space: 58.1 MB (13.51%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,38 +1,29 @@
 class Solution {
-
     public int longestSubarray(int[] nums, int limit) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-            (a, b) -> b[0] - a[0]
-        );
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-            Comparator.comparingInt(a -> a[0])
-        );
-
-        int left = 0, maxLength = 0;
-
-        for (int right = 0; right < nums.length; ++right) {
-            maxHeap.offer(new int[] { nums[right], right });
-            minHeap.offer(new int[] { nums[right], right });
-
-            // Check if the absolute difference between the maximum and minimum values in the current window exceeds the limit
-            while (maxHeap.peek()[0] - minHeap.peek()[0] > limit) {
-                // Move the left pointer to the right until the condition is satisfied.
-                // This ensures we remove the element causing the violation
-                left = Math.min(maxHeap.peek()[1], minHeap.peek()[1]) + 1;
-
-                // Remove elements from the heaps that are outside the current window
-                while (maxHeap.peek()[1] < left) {
-                    maxHeap.poll();
-                }
-                while (minHeap.peek()[1] < left) {
-                    minHeap.poll();
-                }
+        Deque<Integer> maxDeque = new LinkedList<Integer>();
+        Deque<Integer> minDeque = new LinkedList<Integer>();
+        int start = 0;
+        int res = 0;
+        for(int end=0;end<nums.length;end++){
+            while(!maxDeque.isEmpty() && maxDeque.peekLast()<nums[end]){
+                maxDeque.pollLast();
             }
-
-            // Update maxLength with the length of the current valid window
-            maxLength = Math.max(maxLength, right - left + 1);
+             maxDeque.offerLast(nums[end]);
+            while(!minDeque.isEmpty() && minDeque.peekLast()>nums[end]){
+                minDeque.pollLast();
+            }
+            minDeque.offerLast(nums[end]);
+            while(maxDeque.peekFirst()-minDeque.peekFirst()>limit){
+                if(maxDeque.peekFirst()==nums[start]){
+                    maxDeque.pollFirst();
+                }
+                if(minDeque.peekFirst()==nums[start]){
+                    minDeque.pollFirst();
+                }
+                start++;
+            }
+            res=Math.max(res,end-start+1);
         }
-
-        return maxLength;
+        return res;
     }
 }
