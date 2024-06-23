28f53539f26ea5398115ea39f08dc33a6b6f2271
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-06-23 14:37:40 +0530

    Time: 4 ms (40.07%), Space: 45.7 MB (96.21%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int sum=0,impact=0, imp=0;
-        for(int i=0;i<customers.length;i++){
-            if(grumpy[i]==0) sum+=customers[i];
-        }
         for(int i=0;i<minutes;i++){
+             if(grumpy[i]==0) sum+=customers[i];
             if(grumpy[i]==1) imp+=customers[i];
         }
         impact=Math.max(impact,imp);
         for(int i=minutes;i<customers.length;i++){
+             if(grumpy[i]==0) sum+=customers[i];
               if(grumpy[i]==1) imp+=customers[i];
               if(grumpy[i-minutes]==1)  imp-=customers[i-minutes];
              impact=Math.max(impact,imp);
