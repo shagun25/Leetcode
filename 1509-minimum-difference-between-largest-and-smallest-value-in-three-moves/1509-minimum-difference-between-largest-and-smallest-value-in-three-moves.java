5227cfc6a39a077246bfccec2c979c4019804255
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-07-03 22:18:57 +0530

    Time: 3 ms (99.48%), Space: 57.2 MB (29.92%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,36 +1,60 @@
 public class Solution {
 
     public int minDifference(int[] nums) {
-        int numsSize = nums.length;
-        if (numsSize <= 4) {
-            return 0;
-        }
+        if (nums.length <= 4) return 0;
+        if (nums.length == 5) return 1;
+        
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int min3 = Integer.MAX_VALUE;
+        int min4 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int max4 = Integer.MIN_VALUE;
+        for (int val : nums) {
+            if (min1 > val) {
+                min4 = min3;
+                min3 = min2;
+                min2 = min1;
+                min1 = val;
+            } else if (min2 > val) {
+                min4 = min3;
+                min3 = min2;
+                min2 = val;
+            } else if (min3 > val) {
+                min4 = min3;
+                min3 = val;
+            } else if (min4 > val) {
+                min4 = val;
+            }
 
-        List<Integer> smallestFour = heapify(new PriorityQueue<>(
-            Collections.reverseOrder()
-        ), nums);
-
-        List<Integer> largestFour = heapify(new PriorityQueue<>(), nums);
-
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 0; i < 4; i++) {
-            minDiff = Math.min(
-                minDiff,
-                largestFour.get(i) - smallestFour.get(i)
-            );
-        }
-
-        return minDiff;
-    }
-    List<Integer> heapify(PriorityQueue<Integer> heap, int[] nums){
-        for (int num : nums) {
-            heap.offer(num);
-            if (heap.size() > 4) {
-                heap.poll();
+            if (max1 < val) {
+                max4 = max3;
+                max3 = max2;
+                max2 = max1;
+                max1 = val;
+            } else if (max2 < val) {
+                max4 = max3;
+                max3 = max2;
+                max2 = val;
+            } else if (max3 < val) {
+                max4 = max3;
+                max3 = val;
+            } else if (max4 < val) {
+                max4 = val;
             }
         }
-        List<Integer> list = new ArrayList<>(heap);
-        Collections.sort(list);
-        return list;
+
+        int[] minmax = new int[] {min1, min2, min3, min4, max4, max3, max2, max1};
+
+        int left = 0;
+        int right = minmax.length - 4;
+        int diff = minmax[right] - minmax[left];
+        for (int i = 0; i < 3; i++) {
+            diff = Math.min(diff, minmax[++right] - minmax[++left]);
+        }
+
+        return diff;
     }
 }
