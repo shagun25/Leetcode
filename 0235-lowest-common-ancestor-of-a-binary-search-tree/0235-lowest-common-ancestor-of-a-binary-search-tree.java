194e86c8b4685f9ff58e382f2e9ef360a4d340af
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-07-05 01:38:33 +0530

    Time: 5 ms (100.00%), Space: 44.4 MB (95.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,11 +10,9 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root.val==p.val || root.val==q.val) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left!=null && right!=null) return root; // Case1: p and q lies in left and right or vice versa.
-        if(left!=null) return left; // Case2: p and q both lies on left side, whichever you find first will be the ans.
-        return right;   // Case3: p and q both lies on right side, whichever you find first will be the ans.
+        if(root==null) return root;
+        if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left, p, q);// Case3: p and q both lies on right side, whichever you find first will be the ans.
+        else if(root.val<p.val && root.val<q.val) return lowestCommonAncestor(root.right, p, q);// Case2: p and q both lies on left side, whichever you find first will be the ans.
+        else return root;  // Case1: p and q lies in left and right or vice versa.
     }
 }
