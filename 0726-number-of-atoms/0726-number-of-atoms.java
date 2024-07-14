c795b15ab579d51544f49a81a97d0c50d65a0331
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-07-14 23:30:38 +0530

    Time: 26 ms (6.33%), Space: 44.9 MB (5.20%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.java
+++ 0726-number-of-atoms/0726-number-of-atoms.java
@@ -10,12 +10,11 @@ class Solution {
                 st.push(str);
             }
             else if(!st.isEmpty() && Character.isDigit(f) && st.peek().equals(")")){
-                int val = Character.getNumericValue(f); //2
+                int val = Character.getNumericValue(f); 
                 while(i+1<formula.length() && Character.isDigit(formula.charAt(i+1))) {
                     val=val*10+formula.charAt(i+1)-'0';
                     i++;
                 }
-                System.out.println("val: "+val);
                 st.pop();
                  Stack<String> st1 = new Stack<>();
                 while(!st.isEmpty() && !st.peek().equals("(")){
@@ -28,31 +27,30 @@ class Solution {
                         st1.push(st.pop());
                     }
                 }
-                 //System.out.println(st.peek());
+
                 st.pop();
                 while(!st1.isEmpty()){
-                    //System.out.println(st1.peek());
+
                     st.push(st1.pop());
                 }
             }else{
                 if(Character.isDigit(f)){
-                     int val = Character.getNumericValue(f); //2
+                     int val = Character.getNumericValue(f); 
                      while(i+1<formula.length() && Character.isDigit(formula.charAt(i+1))) {
                          val=val*10+formula.charAt(i+1)-'0';
                          i++;
                      }
-                    System.out.println("val: "+val);
                     st.push(String.valueOf(val));
                 }
                  else st.push(String.valueOf(f));
             }
        }
-        //System.out.println(st.size());
        TreeMap<String,Integer> map = new TreeMap<>();
        
        while(!st.isEmpty()){
-          // System.out.println(st.peek());
-           if(st.peek().matches(".*\\d.*")){
+            if(st.peek().equals("(") ||st.peek().equals(")") ){
+             st.pop();
+           }else if(st.peek().matches(".*\\d.*")){
                int val = Integer.valueOf(st.pop());
                String key = st.pop(); 
                if(map.containsKey(key))map.put(key,map.get(key)+val);
@@ -63,13 +61,9 @@ class Solution {
               else map.put(key,1);
            }
        }
-      // System.out.println(st.size());
+
         String str = "";
         for (Map.Entry<String,Integer> entry : map.entrySet()) {
-            if(entry.getKey().equals("(") ||entry.getKey().equals(")") ){
-              continue;
-           }
-            //System.out.println("key: "+ entry.getKey()+ " "+ "value: "+entry.getValue());
             if(entry.getValue()==1) str+=entry.getKey();
             else str+=entry.getKey()+Integer.toString(entry.getValue());
         }
