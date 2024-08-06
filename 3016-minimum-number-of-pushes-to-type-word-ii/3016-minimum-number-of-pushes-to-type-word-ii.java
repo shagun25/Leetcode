6c4f83e3b63eb7289a7331d053c2f2337b5af9af
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-08-07 01:50:32 +0530

    Time: 8 ms (91.94%), Space: 45.7 MB (45.68%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,26 +1,31 @@
-class Solution {
+public class Solution {
 
     public int minimumPushes(String word) {
-        // Frequency map to store count of each letter
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        // Frequency array to store count of each letter
+        int[] frequency = new int[26];
 
         // Count occurrences of each letter
         for (char c : word.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            frequency[c - 'a']++;
         }
 
-        // Priority queue to store frequencies in descending order
-        PriorityQueue<Integer> frequencyQueue = new PriorityQueue<>(
-            (a, b) -> b - a
-        );
-        frequencyQueue.addAll(frequencyMap.values());
+        // Sort frequencies in descending order
+        Arrays.sort(frequency);
+
+        /*
+        // Or do like this 
+        // Sort frequencies in descending order
+        Integer[] sortedFrequency = Arrays.stream(frequency).boxed().toArray(Integer[]::new);
+        Arrays.sort(sortedFrequency, (a, b) -> b - a);
+        */
 
         int totalPushes = 0;
-        int index = 0;
 
         // Calculate total number of presses
-        while (!frequencyQueue.isEmpty()) {
-            totalPushes += (index / 8 + 1) * frequencyQueue.poll();
+        int index=0;
+        for (int i = 25; i >=0; i--) {
+            if (frequency[i] == 0) break;
+            totalPushes += (index / 8 + 1) * frequency[i];
             index++;
         }
 
