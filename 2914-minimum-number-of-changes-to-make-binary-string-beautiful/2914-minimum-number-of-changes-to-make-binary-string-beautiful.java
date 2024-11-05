ff9444772d9a2697199ae44f8db5ff08d1680501
Shagun goyal <GOYALSHAGUN25@GMAIL.COM>
2024-11-05 10:51:30 +0530

    Time: 3 ms (97.78%), Space: 45.3 MB (7.03%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int minChanges(String s) {
+        int minChangesRequired = 0;
+
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                minChangesRequired++;
+            }
+        }
+
+        return minChangesRequired;
+    }
+}
